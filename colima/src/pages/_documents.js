c9ae9bf5f5c9b57a629d58da7a6763551a8d136f
Guilherme Humberto <gbreu@outlook.com>
2020-12-30 01:12:05 -0300

    Template Site Estático (Colima)

--- colima/src/pages/_documents.js
+++ colima/src/pages/_documents.js
@@ -1,11 +1,8 @@
-import React from "react";
-
-import Document, { Html, Head, Main, NextScript } from "next/document";
-
-import { ServerStyleSheet } from "styled-components";
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps() {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -29,26 +26,5 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
-  }
-
-  // Configurando Header global da aplicação
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta charSet="utf-8" />
-
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
   }
 }
